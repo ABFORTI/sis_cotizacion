a4e906146e6af9ef1afa06d84413dcab47a24a33
ABFORTI <soporte@ab-forti.com>
2026-03-05 16:04:13 -0600

    [UPDATE] Margen para el resumes de costos

--- cotizaciones/resources/views/components/footer.blade.php
+++ cotizaciones/resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="footer-container">
-        <p class>&copy; 2025 Cotizaciones. Todos los derechos reservados.</p>
+        <p class>&copy; 2026 Cotizaciones. Todos los derechos reservados.</p>
         <div class="flex items-start justify-between mb-4 header-mobile">
             <img src="{{ asset('images/innovet-logo.png') }}" alt="Innovet">
         </div>
